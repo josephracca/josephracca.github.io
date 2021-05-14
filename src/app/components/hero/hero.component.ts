50f6a4946c713d4d8b783a02aff06513ab605a10
josephracca <josephracca@gmail.com>
2021-05-14 13:36:39 -0700

    tweaks

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,12 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+  action: string = "";
+
   headshot: string = "./../../images/headshot.png"
 
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+    this.changeActionWord();
   }
+
+  doSomethingCool(elementId: string): void {
+    console.log("go down");
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
+
+  changeActionWord() {
+
+    setInterval(() => {
+      //replaced function() by ()=>
+      // this.myDate = new Date();
+      //now iterate through array
+      let actionWordsList = ["click", "tap", "scroll"]
+      console.log("test");
+
+      this.action = actionWordsList[Math.floor(Math.random() * actionWordsList.length)]
+      console.log(this.action);
+      // just testing if it is working
+      // return "string";
+    }, 1700);
+
+    
+  }
+
+
+
+  
 
 }
